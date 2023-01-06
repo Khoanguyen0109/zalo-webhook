a92ec9e88646dccd8aa852ddf1fa9d76f9d0b298
Khoa Nguyen <khoa.nguyen@codestringers.com>
2023-01-06 19:05:05 +0700

    restruct11

--- src/app.js
+++ src/app.js
@@ -22,8 +22,8 @@ app.use(bodyParser.json({ limit: '50mb' }));
 // Load the routes ("controllers" -ish)
 // Setup routes here
 
-// app.get('/api/webhook', webhook);
-// app.post('/api/webhook', webhook);
+app.get('/api/webhook', webhook);
+app.post('/api/webhook', webhook);
 
 app.get('/', async (req, res) => {
   res.json({ status: true, message: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL });
