74400227846e610ef42ed39a9d2efedefd8693cc
Khoa Nguyen <khoa.nguyen@codestringers.com>
2023-02-02 16:26:38 +0700

    update api 9

--- src/app.js
+++ src/app.js
@@ -46,20 +46,14 @@ app.post('/api/webhook', async (req, res) => {
     const info = await doc.loadInfo(); // loads document properties and worksheets
     const sheet = doc.sheetsByIndex[0];
 
-    if (req?.body?.event_name) {
-      switch (req.body.event_name) {
-        case 'user_send_text':
-          await sheet.addRows([
-            {
-              event: req.body?.event_name,
-              userId: req.body?.sender?.id,
-              message: req.body?.message?.text,
-            },
-          ]);
-          break;
-        default:
-          break;
-      }
+    if (req?.body?.event_name === 'user_send_text') {
+      await sheet.addRows([
+        {
+          event: req.body?.event_name,
+          userId: req.body?.sender?.id,
+          message: req.body?.message?.text,
+        },
+      ]);
     }
     return res.status(200).json({ message: 'webhook' });
   } catch (error) {
