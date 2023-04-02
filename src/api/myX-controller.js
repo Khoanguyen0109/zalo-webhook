2859a5b275d1a39a85a946b91a770e4063d672e5
Khoa Nguyen <khoanguyen0109.99@gmail.com>
2023-04-02 15:40:50 +0700

    myxTeam integrate

--- src/api/myX-controller.js
+++ src/api/myX-controller.js
@@ -1,7 +1,114 @@
+const { default: axios } = require("axios");
 const express = require("express");
+const https = require("https");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Birds home page");
+router.post("/", async (req, res) => {
+  try {
+    const {
+      ProjectId,
+      SectionName,
+      EmailAssigned,
+      TaskName,
+      Description,
+      StartDate,
+      EndDate,
+    } = req.body;
+
+    const deviceId = "b766d6b-f120-454e-bafd-60f00ad23fe9111";
+    const username = process.env.EMAIL_MY_X_TEAM_NOI_THAT;
+    const password = process.env.PASSWORD_MY_X_TEAM_NOI_THAT;
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: false,
+    });
+    const myXEndpoint = "https://apiv2.myxteam.com";
+    const {
+      data: {
+        data: { AccessToken },
+      },
+    } = await axios.post(
+      `${myXEndpoint}/auth/login`,
+      {
+        username,
+        password,
+      },
+      {
+        headers: {
+          deviceid: deviceId,
+        },
+        httpsAgent,
+      }
+    );
+    const headers = {
+      Authorization: `Bearer ${AccessToken}`,
+    };
+    const resProject = await axios.post(
+      `${myXEndpoint}/projects/getProject`,
+      {
+        ProjectId,
+      },
+      {
+        headers,
+        httpsAgent,
+      }
+    );
+    if (!resProject.data.data.ProjectId) {
+      res.sendStatus(400).json("Project not found");
+    }
+
+    const [resSection, resTeamMember] = await Promise.all([
+      axios.post(
+        `${myXEndpoint}/sections/getSections`,
+        {
+          IncludeArchived: true,
+          ProjectId: 372254,
+        },
+        {
+          headers,
+          httpsAgent,
+        }
+      ),
+      axios.post(
+        `${myXEndpoint}/projects/getProjectMembers`,
+        {
+          IncludeArchived: true,
+          ProjectId: 372254,
+        },
+        {
+          headers,
+          httpsAgent,
+        }
+      ),
+    ]);
+    const section = resSection.data.data.find(
+      (item) => item.SectionName === SectionName
+    )?.SectionId;
+    const assigned = resTeamMember.data.data.find(
+      (item) => item.User.Email === EmailAssigned
+    )?.UserId;
+
+    const resPost = await axios.post(
+      `${myXEndpoint}/tasks/createTask`,
+      {
+        ProjectId,
+        TaskName,
+        SectionId: section,
+        AssignedId: assigned,
+        Description: `<p>${Description}</p>`,
+        StartDateUnix: Math.floor(new Date(StartDate).getTime() / 1000),
+        DueDateUnix: Math.floor(new Date(EndDate).getTime() / 1000),
+        Followers: [],
+        IsComplete: false,
+      },
+      {
+        headers,
+        httpsAgent,
+      }
+    );
+    res.send("API Success");
+  } catch (error) {
+    console.log("error :>> ", error);
+    res.sendStatus(500);
+  }
 });
 module.exports = router;
