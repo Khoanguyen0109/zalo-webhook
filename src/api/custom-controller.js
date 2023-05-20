3d0432abd3d9fde4b600cb67647b191f30d15ce9
khoa.nguyen <khoanguyen0109.99@gmail.com>
2023-05-20 22:47:30 +0700

    copy

--- src/api/custom-controller.js
+++ src/api/custom-controller.js
@@ -3,31 +3,59 @@ const router = express.Router();
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const { format, toDate } = require("date-fns");
 const moment = require("moment-timezone");
+const {
+  getAuthToken,
+  getSpreadSheet,
+  getSpreadSheetValues,
+  updateSheetValues,
+} = require("../services/googleSheetsService");
 
 router.post("/copy-sheet-data", async (req, res) => {
+  // try {
+  //   const { spreadSheetId, sheetName, rowStart, limit } = req.body;
+  //   const doc = new GoogleSpreadsheet(spreadSheetId);
+  //   console.log('rowStart', rowStart)
+  //   await doc.useServiceAccountAuth({
+  //     //   client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL_CUSTOM,
+  //     //   private_key: process.env.GOOGLE_PRIVATE_KEY_CUSTOM,
+  //     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL_RENDER_FORM,
+  //     private_key: process.env.GOOGLE_PRIVATE_KEY_RENDER_FORM,
+  //   });
+  //   await doc.loadInfo(); // loads document properties and worksheets
+  //   const sheet = doc.sheetsByTitle[sheetName]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+  //   const data = [];
+  //   await( await sheet.getRows({
+  //     offset: rowStart,
+  //     limit: limit ?? 10,
+  //   })).map(item => data.push(item._rawData));
+
+  //   return res.status(200).json({ data: data });
+  // } catch (error) {
+  //   console.log('error', error)
+  //   res.sendStatus(500)
+
+  // }
   try {
-    const { spreadSheetId, sheetName, rowStart, limit } = req.body;
-    const doc = new GoogleSpreadsheet(spreadSheetId);
-    console.log('rowStart', rowStart)
-    await doc.useServiceAccountAuth({
-      //   client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL_CUSTOM,
-      //   private_key: process.env.GOOGLE_PRIVATE_KEY_CUSTOM,
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL_RENDER_FORM,
-      private_key: process.env.GOOGLE_PRIVATE_KEY_RENDER_FORM,
+    const { spreadsheetId, rangeRead, spreadsheetIdWrite, rangeWrite } =
+      req.body;
+
+    const auth = await getAuthToken();
+    const response = await getSpreadSheetValues({
+      spreadsheetId,
+      auth,
+      range: rangeRead,
     });
-    await doc.loadInfo(); // loads document properties and worksheets
-    const sheet = doc.sheetsByTitle[sheetName]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-    const data = [];
-    await( await sheet.getRows({
-      offset: rowStart,
-      limit: limit ?? 10,
-    })).map(item => data.push(item._rawData));
-
-    return res.status(200).json({ data: data });
+    const valueRead = response.data.values;
+    updateSheetValues({
+      spreadsheetId: spreadsheetIdWrite,
+      auth,
+      range: rangeWrite,
+      values: valueRead,
+    });
+    console.log('valueRead', valueRead)
+    return res.status(200).json({ data: response.data });
   } catch (error) {
-    console.log('error', error)
-    res.sendStatus(500)
-
+    console.log("error", error);
   }
 });
 
---
+++ src/services/googleSheetsService.js
@@ -0,0 +1,50 @@
+const { google } = require("googleapis");
+const sheets = google.sheets("v4");
+
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+
+async function getAuthToken() {
+  const auth = new google.auth.GoogleAuth({
+    scopes: SCOPES,
+  });
+  const authToken = await auth.getClient();
+  return authToken;
+}
+
+async function getSpreadSheet({ spreadsheetId, auth }) {
+  const res = await sheets.spreadsheets.get({
+    spreadsheetId,
+    auth,
+  });
+  return res;
+}
+
+async function getSpreadSheetValues({ spreadsheetId, auth, range }) {
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId,
+    auth,
+    range,
+  });
+  return res;
+}
+
+async function updateSheetValues({ spreadsheetId, auth, range, values }) {
+  const res = await sheets.spreadsheets.values.update({
+    spreadsheetId,
+    auth,
+    range,
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      range,
+      values
+    },
+  });
+  return res
+}
+
+module.exports = {
+  getAuthToken,
+  getSpreadSheet,
+  getSpreadSheetValues,
+  updateSheetValues
+};
