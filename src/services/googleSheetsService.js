17a8765c6fb9678472b4bf26c0c585393ed683cf
Nguyễn Hòa <77116057+NguyenHoadev@users.noreply.github.com>
2023-05-31 13:28:39 +0700

    Update googleSheetsService.js

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -32,7 +32,7 @@ async function updateSheetValues({ spreadsheetId, auth, range, values }) {
   await sheets.spreadsheets.values.clear({
       spreadsheetId,
       auth,
-      range
+      "getData!A4:J100"
     });
   
   const res = await sheets.spreadsheets.values.update({
