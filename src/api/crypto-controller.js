38f0a41720411634b4863bdece4e0aca9f50d5de
khoa.nguyen <khoanguyen0109.99@gmail.com>
2023-09-17 16:03:35 +0700

    upload and cost

--- src/api/crypto-controller.js
+++ src/api/crypto-controller.js
@@ -3,6 +3,21 @@ const router = express.Router();
 var constants = require("../constants");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+
+const { google } = require("googleapis");
+const upload = require("../upload");
+
+const credentials = require("../key/crypto-usdt-530bf5e8d3c1.json");
+
+// Create a new JWT client with the credentials
+const auth = new google.auth.JWT(
+  credentials.client_email,
+  null,
+  credentials.private_key,
+  ["https://www.googleapis.com/auth/drive"]
+);
+const drive = google.drive({ version: "v3", auth });
 
 router.get("/currency-exchange", async (req, res, next) => {
   try {
@@ -44,11 +59,32 @@ router.get("/bank-account", async (req, res, next) => {
       bank: item.bank,
       account: item.account,
       account_name: item.account_name,
-      limit: item.limit,
+      status: item.status,
       qr_code: item.qr_code,
       transfer_info: item.transfer_info,
     }));
-    console.log("rows", rows);
+    return res.status(200).json({ data: rows });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/cost", async (req, res, next) => {
+  try {
+    const doc = new GoogleSpreadsheet(constants.CRYPTO_SHEET_ID);
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL_CRYPTO,
+      private_key: process.env.GOOGLE_PRIVATE_KEY_CRYPTO,
+    });
+    await doc.loadInfo(); // loads document properties and worksheets
+    const sheet = doc.sheetsByTitle["extend_cost"]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+    const rows = await (
+      await sheet.getRows()
+    ).map((item) => ({
+      currency: item.currency,
+      buy_fee: item.buy_fee,
+      deposit_fee: item.deposit_fee
+    }));
     return res.status(200).json({ data: rows });
   } catch (error) {
     next(error);
@@ -69,7 +105,7 @@ router.get("/wallet", async (req, res, next) => {
     ).map((item) => ({
       wallet_address: item.wallet_address,
       network: item.network,
-      limit: item.limit,
+      status: item.status,
     }));
     console.log("rows", rows);
     return res.status(200).json({ data: rows });
@@ -96,7 +132,7 @@ router.post("/crypto_buy", async (req, res, next) => {
       private_key: process.env.GOOGLE_PRIVATE_KEY_CRYPTO,
     });
     await doc.loadInfo(); // loads document properties and worksheets
-    const sheet = doc.sheetsByTitle["buy_record"]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+    const sheet = doc.sheetsByTitle["deposit_record"]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
     await sheet.addRow({
       id,
       currency,
@@ -123,7 +159,7 @@ router.post("/crypto_buy_usdt", async (req, res, next) => {
       private_key: process.env.GOOGLE_PRIVATE_KEY_CRYPTO,
     });
     await doc.loadInfo(); // loads document properties and worksheets
-    const sheet = doc.sheetsByTitle["buy_record_usdt"]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+    const sheet = doc.sheetsByTitle["deposit_record_usdt"]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
     await sheet.addRow({
       id,
       currency,
@@ -182,4 +218,48 @@ router.post("/crypto_sell", async (req, res, next) => {
     next(error);
   }
 });
+
+router.post("/upload", upload.single("image"), (req, res, next) => {
+  try {
+    const folderId = "1nz1bv3kdnqr5Sgy1S6iM_E8htsywstPI";
+    // File details
+    const fileMetadata = {
+      name: req.file.originalname,
+      parents: [folderId],
+      // Add any additional metadata you want to specify
+    };
+
+    // Read the image file
+    const media = {
+      mimeType: req.file.mimetype,
+      body: fs.createReadStream(req.file.path),
+    };
+
+    // Upload the file to Google Drive
+    drive.files.create(
+      {
+        resource: fileMetadata,
+        media: media,
+        fields: "id",
+      },
+      (err, file) => {
+        if (err) {
+          console.error("Error uploading file:", err);
+          res.status(500).send("Error uploading file");
+          return;
+        }
+
+        console.log("File uploaded successfully. File ID:", file.data.id);
+
+        // Delete the temporary file
+        fs.unlinkSync(req.file.path);
+
+        res.send("File uploaded successfully to Google Drive");
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
